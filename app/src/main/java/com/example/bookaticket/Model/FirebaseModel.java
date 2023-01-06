85cc9b4dc165e2b1b92b9207fe815b9362b4917c
amitsabag7 <amitsabag752@gmail.com>
2023-01-06 18:36:48 +0200

    fix directories hierarchy

--- app/src/main/java/com/example/bookaticket/Model/FirebaseModel.java
+++ app/src/main/java/com/example/bookaticket/Model/FirebaseModel.java
@@ -1,19 +1,13 @@
-package model;
+package com.example.bookaticket.Model;
 
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
-import com.example.bookaticket.Login_Fragment;
-import com.example.bookaticket.MainActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import java.util.concurrent.Executor;
 
 
 public class FirebaseModel {
@@ -24,7 +18,7 @@ public class FirebaseModel {
         mAuth = FirebaseAuth.getInstance();
     }
 
-    public void loginUser(String email, String password,Model.LoginListener callback){
+    public void loginUser(String email, String password, Model.LoginListener callback){
         mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
